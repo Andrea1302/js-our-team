a4bc47bdebd37155857b33ac2710721e9665420a
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-04 19:06:09 +0100

    Esercizio con funzioni

--- js/script2.js
+++ js/script2.js
@@ -45,77 +45,58 @@ const cards = [
     }
 
 ]
+// eseguo funzione 
+addMembers();
 
+aggiungi.addEventListener("click",function(){
+    let oggettoDaAggiungere = aggiungiMembro(nomeInput.value,immagineInput.value,ruoloInput.value);
 
-for ( let i =  0 ; i < cards.length ; i++){
-    let objectMember = cards[i];
-    let nomeMembro = objectMember.nome;
-    let ruoloMembro = objectMember.ruolo;
-    let immagineMembro = objectMember.immagine
+    containerTeam.innerHTML += oggettoDaAggiungere;
 
-    // creo stringa inner dove scriverò l html da inserire all interno del mio team container
-    let innerMembers = ` 
-    <div class="team-card">
-        <div class="card-image">
-        <img
-            src="${immagineMembro}"
-            alt="${nomeMembro}"
-        />
-        </div>
-        <div class="card-text">
-        <h3>${nomeMembro}</h3>
-        <p>${ruoloMembro}</p>
-        </div>
-    </div>`
-    // richiamo la variabile dove vorrò inserire questo html 
-    containerTeam.innerHTML += innerMembers;
-}
-
-aggiungi.addEventListener("click", function(){
-    let NomeMembroAdd = nomeInput.value;
-    let ruoloMembroAdd = ruoloInput.value;
-    let immagineMembroAdd = immagineInput.value;
-
-    let newMembers = {
-        "nome" : NomeMembroAdd,
-        "immagine" : immagineMembroAdd,
-        "ruolo" : ruoloMembroAdd
-    }
-    cards.push(newMembers)
-    let NewobjectMember = cards[cards.length - 1];
-    let nomeMembro = NewobjectMember.nome;
-    let ruoloMembro = NewobjectMember.ruolo;
-    let immagineMembro = NewobjectMember.immagine
-    
-    // creo stringa inner dove scriverò l html da inserire all interno del mio team container
-    let innerMembers = ` 
-    <div class="team-card">
-        <div class="card-image">
-        <img
-            src="${immagineMembro}"
-            alt="${nomeMembro}"
-        />
-        </div>
-        <div class="card-text">
-        <h3>${nomeMembro}</h3>
-        <p>${ruoloMembro}</p>
-        </div>
-    </div>`
-    // richiamo la variabile dove vorrò inserire questo html 
-    containerTeam.innerHTML += innerMembers;
-// }
 })
-let NomeMembroAdd = nomeInput.value;
-let ruoloMembroAdd = ruoloInput.value;
-let immagineMembroAdd = immagineInput.value;
 
+
+
+
+
+
+
+
+
+// Funzione per inserire il mio array di oggetti nell html 
+function addMembers (){
+    for ( let i =  0 ; i < cards.length ; i++){
+        let objectMember = cards[i];
+        let nomeMembro = objectMember.nome;
+        let ruoloMembro = objectMember.ruolo;
+        let immagineMembro = objectMember.immagine
+    
+        // creo stringa inner dove scriverò l html da inserire all interno del mio team container
+        let innerMembers = ` 
+        <div class="team-card">
+            <div class="card-image">
+            <img
+                src="${immagineMembro}"
+                alt="${nomeMembro}"
+            />
+            </div>
+            <div class="card-text">
+            <h3>${nomeMembro}</h3>
+            <p>${ruoloMembro}</p>
+            </div>
+        </div>`
+        // richiamo la variabile dove vorrò inserire questo html 
+        containerTeam.innerHTML += innerMembers;
+    }
+}
+// Funzione aggiungi membro 
 function aggiungiMembro(nomeMembroToAdd,immagineToAdd,ruoloToAdd){
     
 
     let newMembers = {
-        "nome" : NomeMembroAdd,
-        "immagine" : immagineMembroAdd,
-        "ruolo" : ruoloMembroAdd
+        "nome" : nomeMembroToAdd,
+        "immagine" : immagineToAdd,
+        "ruolo" : ruoloToAdd
     }
     cards.push(newMembers)
     let NewobjectMember = cards[cards.length - 1];
@@ -138,5 +119,5 @@ function aggiungiMembro(nomeMembroToAdd,immagineToAdd,ruoloToAdd){
         </div>
     </div>`
     // richiamo la variabile dove vorrò inserire questo html 
-    containerTeam.innerHTML += innerMembers;
+   return innerMembers;
 }
