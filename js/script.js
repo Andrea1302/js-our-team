3eb4e7de33e62f574b4852bfc4c9082a87e8e552
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-04 14:27:23 +0100

    Mia logica esericizio

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* Ricreiamo le card del nostro team aggiungendo al layout di base fornito, il nostro javascript in cui:
+- Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. (nel nostro file js)
+Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+- Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+*/
+
+// 1 creo oggetto  che dovrà contenere nome, immagine e ruolo 
+
+// 2. creo un array dove all interno inserirò gli oggetti da me creati inizialmente ( in seguito li aggiungerò attraverso un push );
+
+
+// 4. devo sostituire al posto dell immagine, del nome e del paragrafo , i valori che abbiamo nell oggetto selezionato 
+    // 4.1 Attraverso un ciclo for seleziono l oggetto che voglio e creo le variabile in cui salvo i valori di quell oggetto.
+    // 5 stampo sull html l output finale
+    // 5.1 creo una variabile globale dove poi vorrò inserire l html
+    // 5.2 assegno alla mia variabile una stringa dove inserirò ciò che vorrò nell html
+
+    // 5.3 sostituisco i valori delle variabili create alla relativa parte della stringa che vorrò inserire nell html .
+
+// 6. Creo un oggetto che dovrà essere aggiunto al nostro array di oggetti
+// 7 il valore di quell oggetto sarà generato dall input dell utente
+    //7.1  creo una variabile in cui andararmi a salvare l elementoValue richiesto dopo il click
+    // 7.2 Richiamo il bottone add e lo salvo in una variabile.
+    // 7.3 Al suo click andrò a salvarmi in ogni variabile la sua value.
+    // 7.4 sostituisco la variabile al valore dell oggetto
+// 8. Aggiungo l oggetto al mio array con il push
